6e7e406cbe9f40918e3b745e88e4cd4da5e83e82
wilfredo-flores <willflores1221@gmail.com>
2022-09-30 15:09:04 -0400

    Add interface

---
+++ src/components/SortInterface.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "../App.css";
+import Bar from "./Bar";
+
+interface Props {
+	lengthOfArray: number;
+	generateNewArray: boolean;
+	setGenerateNewArray: React.Dispatch<React.SetStateAction<boolean>>;
+	conductSort: string;
+	setConductSort: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function SortInterface(props: Props) {
+	const [arrayToSort, setArrayToSort] = useState(() => {
+		let array = [];
+		for (let i = 0; i < 47; i++) {
+			array[i] = { height: 0, sorted: false };
+			array[i].height = Math.floor(Math.random() * 500 + 10);
+			array[i].sorted = false;
+		}
+		return array;
+	});
+
+	return (
+		<div>
+			{arrayToSort.map((bar, index) => {
+				return (
+					<Bar
+						height={bar.height}
+						sorted={bar.sorted}
+						index={index}
+						key={index}
+					/>
+				);
+			})}
+		</div>
+	);
+}
+
+export default SortInterface;
